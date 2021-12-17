864438c0babe1497a47f5900bdcedaa050f5b35e
Hesham222 <heshamashraf971@gmail.com>
2021-12-17 15:47:09 +0200

    adding languages in url

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,7 +1,16 @@
 <?php
 
-Route::prefix('dashboard')->name('dashboard.')->group(function(){
 
-    Route::get('/index','DashboardController@index')->name('index');
-});
+Route::group(
+    ['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]],
+    
+    function(){
+
+        Route::prefix('dashboard')->name('dashboard.')->group(function(){
+
+            Route::get('/index','DashboardController@index')->name('index');
+        });
+    });
+
+
 
